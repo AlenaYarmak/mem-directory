e01888cd77e024ef8ae9364ae606e2931b35a211
Alena <olena.yarmak.mail@gmail.com>
2025-04-17 09:00:55 +0200

    create test table

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import Table from './components/CustomTable';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Mem directory</h1>
+      <Table />
     </div>
   );
 }
